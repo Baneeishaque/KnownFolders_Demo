941f45591709453871c0859902acd437075c6571
Banee Ishaque K <baneeishaque@gmail.com>
2020-07-24 02:39:05 +0530

    last

--- KnownFolders_Demo/Program.cs
+++ KnownFolders_Demo/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Syroot.Windows.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace KnownFolders_Demo
@@ -9,6 +11,32 @@ namespace KnownFolders_Demo
     {
         static void Main(string[] args)
         {
+            // Go through each KnownFolderType enumeration member.
+            foreach (KnownFolderType type in Enum.GetValues(typeof(KnownFolderType)))
+            {
+                KnownFolder knownFolder = new KnownFolder(type);
+
+                // Write down the current and default path.
+                Console.WriteLine(knownFolder.Type.ToString());
+                try
+                {
+                    Console.Write("Current Path: ");
+                    Console.WriteLine(knownFolder.Path);
+                    Console.Write("Default Path: ");
+                    Console.WriteLine(knownFolder.DefaultPath);
+                }
+                catch (ExternalException ex)
+                {
+                    // While uncommon with personal folders, other KnownFolders don't exist on every system, and trying
+                    // to query those returns an error which we catch here.
+                    Console.WriteLine("<Exception> " + ex.ErrorCode);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Current Downloads Path: " + new KnownFolder(KnownFolderType.Downloads).Path);
+            Console.WriteLine("Default Downloads Path: " + new KnownFolder(KnownFolderType.Downloads).DefaultPath);
+            Console.ReadLine();
         }
     }
 }
